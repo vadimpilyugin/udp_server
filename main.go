254c66bf8499cadf8111aa52b42c339379c163c2
vadimpilyugin <vadimpilyugin@gmail.com>
2018-12-18 19:54:26 +0300

    add args

--- main.go
+++ main.go
@@ -3,12 +3,28 @@ package main
 import (
   "github.com/vadimpilyugin/debug_print_go"
   "net"
+  "os"
 )
 
+const (
+  MTU = 1500
+)
+
+func usage() {
+  println("Usage: main [ADDR]")
+  os.Exit(1)
+}
+
 func main() {
+  if len(os.Args) < 2 {
+    usage()
+  }
   printer.Debug("Hello, world!")
+
+  addr := os.Args[1]
+
   // listen to incoming udp packets
-  pc, err := net.ListenPacket("udp", "127.0.0.1:5553")
+  pc, err := net.ListenPacket("udp", addr)
   if err != nil {
     printer.Fatal(err)
   }
@@ -16,7 +32,7 @@ func main() {
   printer.Debug("Listening...")
 
   //simple read
-  buffer := make([]byte, 1024)
+  buffer := make([]byte, MTU)
   printer.Debug("Waiting for read...")
   pc.ReadFrom(buffer)
 
